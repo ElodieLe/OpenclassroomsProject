a5d8f44019054183c43b0f1608cb84ce0ee66891
John Doe <johndoe@example.com>
2023-02-24 17:00:53 +0100

    création de la branche main

---
+++ Projet/Projet.java
@@ -0,0 +1,15 @@
+package com.openclassrooms;
+
+public class Projet {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i < 5; i++ ) {
+		System.out.println("J'utilise Eclipse !");
+		}
+	}
+	public int ajouter(int num1, int num2) {
+		return num1 + num2;
+	}
+
+}
---
+++ Projet/ProjetTest.java
@@ -0,0 +1,14 @@
+package com.openclassrooms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
